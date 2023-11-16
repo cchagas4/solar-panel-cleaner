6e0dd44bde3a2f5ad02fcb2d292586d4a2ea9868
cassia chagas <cassiansbc@gmail.com>
2023-11-16 00:33:35 +0000

    Added servo motor class

---
+++ cleaner/ServoMotor.cpp
@@ -0,0 +1,15 @@
+#include "ServoMotor.h"  
+
+ServoMotor::ServoMotor()
+{
+
+}
+
+ServoMotor::ServoMotor(byte servoPin)
+{
+    servoMotor.attach(servoPin);
+}
+
+void ServoMotor::write(int degree){
+    servoMotor.write(degree);
+}
